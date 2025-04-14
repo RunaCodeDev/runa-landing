1e9b2656acddc73e50964be89c610927440e7937
Irala Agustin <damian.irala@outlook.com>
2025-04-14 09:26:33 -0300

    Adding NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/layout/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="tw-animate-pulse"> HEADER</div>
-        {children}
+        <main className="flex flex-col max-w-7xl mx-auto">
+          <NavBar />
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,3 +1,50 @@
+"use client";
+
+import { Link as ScrollLink } from "react-scroll";
+import Image from "next/image";
+
+interface NavItem {
+  label: string;
+  to: string;
+}
+
+const navItems: NavItem[] = [
+  { label: "Inicio", to: "home" },
+  { label: "Servicios", to: "services" },
+  { label: "Sobre Nosotros", to: "about" },
+  { label: "Contacto", to: "contact" },
+];
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className="fixed top-0 left-0 right-0 bg-white/90  backdrop-blur-sm shadow-sm z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-20">
+          <Image
+            src="/logos/runa-logo.png"
+            width={80}
+            height={50}
+            alt="Runa Logo"
+            className="cursor-pointer"
+          />
+          <div className="flex justify-center flex-1 space-x-8">
+            {navItems.map((item) => (
+              <ScrollLink
+                key={item.to}
+                to={item.to}
+                spy={true}
+                smooth={true}
+                offset={-64} // Ajusta este valor según la altura de tu navbar
+                duration={500}
+                className="cursor-pointer inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2 transition-colors border-transparent text-gray-500 hover:text-primary hover:border-primary"
+                activeClass="border-primary-500 text-primary-700"
+              >
+                {item.label}
+              </ScrollLink>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
